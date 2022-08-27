4ba044d563cb18d1aa214f4c41bfa39fd24ef876
Hay Marn <haymarn03@gmail.com>
2022-08-27 16:34:38 +0800

    build own docker image and pushed to ECR

--- demos/flask-sklearn-student-starter/run_docker.sh
+++ demos/flask-sklearn-student-starter/run_docker.sh
@@ -2,11 +2,11 @@
 
 ## Complete the following steps to get Docker running locally
 
-# Step 1:
-# Build image
+# Step 1: Build image
+docker build --tag=demolocal .
 
-# Step 2: 
-# List docker images
+# Step 2: List docker images
+docker image ls
 
-# Step 3: 
-# Run flask app
+# Step 3: Run flask app
+docker run -it demolocal bash
